07336f75c833622090723995eeccf66e5f7b0538
la_4q <la_4q@lacadstation.ktu.local>
2025-09-04 17:20:07 +0300

    final working project

---
+++ xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
@@ -0,0 +1,5 @@
+1756995525 /home/la_4q/Documents/spi/bench/verilog/tst_bench_top.v
+1756995525 /home/la_4q/Documents/spi/rtl/verilog/fifo4.v
+1756995525 /home/la_4q/Documents/spi/bench/verilog/spi_slave_model.v
+1756995525 /home/la_4q/Documents/spi/rtl/verilog/simple_spi_top.v
+1756995525 /home/la_4q/Documents/spi/bench/verilog/wb_master_model.v
